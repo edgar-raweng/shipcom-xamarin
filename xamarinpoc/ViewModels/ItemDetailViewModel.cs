3979100f7ccbd46eea859dad54793047a9cb0076
Ernesto de la Cruz Morales <ernesto.delacruz.morales@gmail.com>
2021-05-28 17:27:47 -0500

    Initial commit
    Added gitignore for xamarin forms, added sqlcipher library, next commit will load the UWP project.

---
+++ xamarinpoc/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using xamarinpoc.Models;
+
+namespace xamarinpoc.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
